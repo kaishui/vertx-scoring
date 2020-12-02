202793de4dcbc9a9e658d92b2f59e116daaba78f
kaishui <kaishuinanhai@gmail.com>
2020-12-03 00:34:45 +0800

    metric

--- src/main/java/com/ali/scoring/service/TraceDataService.java
+++ src/main/java/com/ali/scoring/service/TraceDataService.java
@@ -1,29 +1,27 @@
 package com.ali.scoring.service;
 
 import com.ali.scoring.controller.CommonController;
-import com.hazelcast.collection.ISet;
-import com.hazelcast.multimap.MultiMap;
-import io.vertx.core.Future;
-import io.vertx.core.buffer.Buffer;
+import com.google.common.collect.*;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
-import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.client.HttpResponse;
-import io.vertx.ext.web.client.WebClient;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.Proxy;
-import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TraceDataService implements Runnable {
     private Logger logger = LoggerFactory.getLogger(TraceDataService.class);
+
+    private static ImmutableSetMultimap.Builder<String, String> allMap = ImmutableSetMultimap.<String, String>builder();
+    private static ImmutableSet.Builder<String> badTraceIds = ImmutableSet.<String>builder();
 
     public TraceDataService() {
     }
@@ -35,80 +33,61 @@ public class TraceDataService implements Runnable {
 
     @Override
     public void run() {
-        //Todo: load data
+        //load data
         String port = System.getProperty("server.port", "8080");
         if ("8000".equals(port) || "8001".equals(port)) {
-
+            long startTime = System.nanoTime();
             try {
-               URL url = new URL(getPath());
-                HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection(Proxy.NO_PROXY);
-                InputStream input = httpConnection.getInputStream();
-                BufferedReader bf = new BufferedReader(new InputStreamReader(input));
-                MultiMap<Object, Object> allMap = VertxInstanceService.getHazelcastInstance().getMultiMap("all");
-                ISet<Object> badTraceIds = VertxInstanceService.getHazelcastInstance().getSet("badTraceIds");
-                long startTime = System.nanoTime();
-                String line = null;
-                try {
-                    while ((line = bf.readLine()) != null) {
-//                        CompletableFuture<String> future = new CompletableFuture<>();
-//                        future.complete(line);
-//                        future.thenCompose(lineStr -> {
-//                            CompletableFuture<String> next = new CompletableFuture<>();
-//                            extractLineData(lineStr, allMap, badTraceIds, traceId ->{
-//                                next.complete(traceId);
-//                            });
-//                            return next;
-//                        }).whenComplete((value, ex) ->{
-//                            logger.debug("traceId:{}" + value);
-//                        });
+                //call http api to get trace data
+                HttpRequest request = HttpRequest.newBuilder().uri(URI.create(getPath())).GET().build();
+                //outputstream with lines
+                HttpResponse.BodyHandler<Stream<String>> bodyHandler = HttpResponse.BodyHandlers.ofLines();
+                HttpClient client = HttpClient.newHttpClient();
+                CompletableFuture<HttpResponse<Stream<String>>> future = client.sendAsync(request, bodyHandler);
+                future.thenApply(HttpResponse::body).whenComplete((inputStream, ex) -> {
 
-                    }
-                } catch (Exception e) {
+                    //id length ?
+                    System.out.println("id metric");
+                    IntSummaryStatistics summaryStatistics = inputStream.mapToInt(trace -> trace.indexOf("|"))
+                            .summaryStatistics();
+                    System.out.println(summaryStatistics);
+                    // error=1 start ?
+//                    String errorFlag = new StringBuffer("error=1").reverse().toString();
+//                    String httpNomarl = new StringBuffer("http.status_code=200").reverse().toString();
+//                    String http3 = new StringBuffer("http.status_code=3").reverse().toString();
+//                    String http5 = new StringBuffer("http.status_code=5").reverse().toString();
+//                    String http4 = new StringBuffer("http.status_code=4").reverse().toString();
+//                    ImmutableSet<String> flags = ImmutableSet.of(
+//                            errorFlag, http3, http4, http5, httpNomarl
+//                    );
+//
+//                    flags.parallelStream().forEach(flag ->{
+//                        System.out.println("error code:" + flag);
+//                        IntSummaryStatistics errorStat = inputStream.map(trace -> new StringBuffer(trace).reverse())
+//                                .map(trace -> trace.indexOf(flag)).mapToInt(len -> len).summaryStatistics();
+//
+//                        System.out.println(errorStat);
+//
+//                    });
 
-                } finally {
-                    long endTime = System.nanoTime();
-                    System.out.println("time:" + (endTime - startTime));
-
-
-
-                    //TODO: COUNT checksum
-                    //
-                }
+                    // http_stust
+//                    Multimap<String, String> list = inputStream.collect(Multimaps.toMultimap(trace -> trace.substring(0, trace.indexOf("\\|")),
+//                            val -> val, MultimapBuilder.treeKeys().arrayListValues()::build));
+//                    System.out.println(list.keys().toString());
+//                    logger.debug("list");
+                });
 
             } catch (Exception e) {
-            }
 
-//            WebClient.create(VertxInstanceService.getVertx())
-//                    .get(Integer.parseInt(CommonController.getDataSourcePort()), "localhost", getURI())
-//                    .send().onSuccess(this::handle);
+            } finally {
+                long endTime = System.nanoTime();
+                System.out.println("time:" + (endTime - startTime));
+            }
         }
     }
 
-    private void handle(HttpResponse<Buffer> res) {
-        System.out.println(res.statusCode());
-        Buffer buffer = res.bodyAsBuffer();
-        InputStream inputStream = new ByteArrayInputStream(buffer.getBytes());
-        BufferedReader bf = new BufferedReader(new InputStreamReader(inputStream));
 
-        MultiMap<Object, Object> allMap = VertxInstanceService.getHazelcastInstance().getMultiMap("all");
-        ISet<Object> badTraceIds = VertxInstanceService.getHazelcastInstance().getSet("badTraceIds");
-        long startTime = System.nanoTime();
-        String line = null;
-        try {
-            while ((line = bf.readLine()) != null) {
-//                extractLineData(line, allMap, badTraceIds);
-            }
-        } catch (Exception e) {
-
-        } finally {
-            long endTime = System.nanoTime();
-            System.out.println("time:" + (endTime - startTime));
-            //TODO: COUNT checksum
-            //
-        }
-    }
-
-    private String extractLineData(String line, MultiMap<Object, Object> allMap, ISet<Object> badTraceIds, Consumer<String> result) {
+    private String extractLineData(String line) {
         String[] cols = line.split("\\|");
         String traceId = null;
         if (cols != null && cols.length > 1) {
@@ -127,7 +106,6 @@ public class TraceDataService implements Runnable {
                 }
             }
         }
-        result.accept(traceId);
         return traceId;
     }
 
